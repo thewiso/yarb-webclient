6b129ed3de49b8de9f4f5780fc9be9bf10aa5ef1
thewiso <tschoenebaum@gmail.com>
2019-12-30 10:08:54 +0100

    adding invitation for boards

--- src/components/User/BoardCard/BoardCard.tsx
+++ src/components/User/BoardCard/BoardCard.tsx
@@ -18,28 +18,34 @@ class BoardCard extends React.Component<BoardCardProperties, BoardCardState> {
 		return (
 			<Card>
 				<CardContent>
-					<Typography color="textSecondary">
-						{this.getFormattedDate()}
-					</Typography>
+					<Typography color="textSecondary">{this.getFormattedDate()}</Typography>
 					<Typography variant="h5">{this.props.board.name}</Typography>
 					<Typography color="textSecondary">{this.props.board.columns.map(board => board.name).join(", ")}</Typography>
 				</CardContent>
 				<CardActions>
 					<Button onClick={this.handleOpenClick.bind(this)}>Open</Button>
-					<Button>Invite</Button>
+					<Button href={this.getMailTo()}>Invite</Button>
 				</CardActions>
 			</Card>
 		);
 	}
 
-	private getFormattedDate(): string{
+	private getFormattedDate(): string {
 		const date = this.props.board.creationDate;
 		//use https://momentjs.com/ ?
 		return date.toLocaleString();
 	}
 
-	handleOpenClick(): void{
+	handleOpenClick(): void {
 		this.props.history.push(`/boards/${this.props.board.id}`);
+	}
+
+	private getMailTo(): string {
+		const subject = encodeURIComponent(`Invitation for Retro Board "${this.props.board.name}"`);
+		const boardUrl = `${window.location.protocol}//${window.location.host}/boards/${this.props.board.id}`;
+		const body = encodeURIComponent(`Hi,\nplease join my YARB retro board: ${boardUrl}`);
+
+		return `mailto:?to=&subject=${subject}&body=${body}`;
 	}
 }
 
